0f022a8e34cb77dee878183c321c83ab523c84c1
xcv58 <chenyihonglove@gmail.com>
2015-03-31 23:39:27 -0400

    update install.zsh

--- install.zsh
+++ install.zsh
@@ -5,16 +5,15 @@ if [ -L $SCRIPT_FILE ]; then
 fi
 SCRIPT_PATH=`dirname $SCRIPT_FILE`
 SCRIPT_FILE=`basename $SCRIPT_FILE`
-# echo Path: $SCRIPT_PATH
-# echo Filename: $SCRIPT_FILE
+cd ${SCRIPT_PATH}
 
 setopt EXTENDED_GLOB
 
 function link() {
     for folder in ${@}; do
         echo
-        echo "Link files in ./${folder}"
-        for file in "${SCRIPT_PATH}/${folder}"/^README.md(.N); do
+        echo "Link files in ${PWD}/${folder}"
+        for file in "${PWD}/${folder}"/^README.md(.N); do
             echo ln -fs "$file" "${ZDOTDIR:-$HOME}/.${file:t}"
             ln -fs "$file" "${ZDOTDIR:-$HOME}/.${file:t}"
         done
